d9a4a3ea15f661b8b740af609c55b5f4bfa808e8
Muhammed Yakub <yakubmuhammed51@gmail.com>
2024-09-13 19:25:31 +0100

    added debug bar for debugging and set up a few side bar settings

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\HtmlMinifier;
 
-Route::view('/', 'welcome')->name('welcome');
+Route::middleware([HtmlMinifier::class])->group(function () {
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    Route::view('/', 'welcome')->name('welcome');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-require __DIR__.'/auth.php';
+    Route::view('profile', 'profile')
+        ->middleware(['auth'])
+        ->name('profile');
+
+    require __DIR__ . '/auth.php';
+});
