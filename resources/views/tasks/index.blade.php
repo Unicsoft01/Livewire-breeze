3782876c72610dad447513021a95d7d5cc96e78c
Muhammed Yakub <yakubmuhammed51@gmail.com>
2024-09-22 21:34:19 +0100

    copied alert to own component

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -388,19 +388,5 @@
         </div>
     </div>
 
-    @script
-        <script>
-            $wire.on('swal', (event) => {
-                const data = event
-                Swal.fire({
-                    title: data[0]['title'],
-                    text: data[0]['message'],
-                    icon: data[0]['icon'],
-                    confirmButtonClass: 'btn btn-success w-sm mt-2',
-                    buttonsStyling: false,
-                    showCloseButton: false
-                })
-            });
-        </script>
-    @endscript
+    @include('components.alerts');
 </div>
